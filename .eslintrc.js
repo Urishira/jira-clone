87ea367ae1929a5332a333da13a0ff3c70e4dde4
uri <urialbertogalvafrancisco@gmail.com>
2022-11-22 02:36:33 -0400

    added first commit prettier and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+	},
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
+	extends: [
+		'plugin:react/recommended',
+		'standard-with-typescript',
+		'eslint-config-prettier',
+	],
+	overrides: [],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['react'],
+	rules: {},
+};
