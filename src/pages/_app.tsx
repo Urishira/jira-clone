d4575b8c55cb7d21911738fe391e422613455873
uri <urialbertogalvafrancisco@gmail.com>
2022-11-22 03:16:18 -0400

    add theme configuration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,11 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ThemeProvider, CssBaseline } from '@mui/material';
-import { theme } from '../theme';
+import { darkTheme, lightTheme } from '../theme';
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<ThemeProvider theme={theme}>
+		<ThemeProvider theme={lightTheme}>
 			<CssBaseline />
 			<Component {...pageProps} />
 		</ThemeProvider>
