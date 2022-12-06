17e012835538099213843ec4d952afce7f65cad6
uri <urialbertogalvafrancisco@gmail.com>
2022-12-06 11:35:12 -0400

    wip somechanges on state

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,17 @@
-import { Typography } from '@mui/material';
+import { Card, CardHeader, Grid, Typography } from '@mui/material';
+import { CardJira } from '../components/common';
+
 import Layout from '../components/layout/Layout';
 
 export default function Home() {
 	return (
 		<div>
-			<Layout>
-				<Typography fontSize={30}>Hello word</Typography>
+			<Layout title='jira-clone'>
+				<Grid container spacing={2}>
+					<CardJira title='In progress'>
+						
+					</CardJira>
+				</Grid>
 			</Layout>
 		</div>
 	);
